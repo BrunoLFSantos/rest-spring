221cea26097da54040a91d7e9d8f4dbebd1c156c
BrunoLFSantos <brunoleonardofsantos@gmail.com>
2022-09-12 20:22:15 -0300

    Primeiros passoa com migração no flyway

--- 01_Spring/rest-spring/src/main/java/br/com/brunosantos/controllers/PersonController.java
+++ 01_Spring/rest-spring/src/main/java/br/com/brunosantos/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.brunosantos.data.vo.v2.PersonVOV2;
 import br.com.brunosantos.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("api/person/v1")
 public class PersonController {
 
 	@Autowired
---
+++ 01_Spring/rest-spring/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` 
+(`id`, `adress`,`first_name`,`gender`,`last_name`)
+VALUES 
+(1,'Petrópolis','Bruno','M','Santos'),
+(2,'Petrópolis','Miguel Ângelo','M','Santos'),
+(6,'Petrópolis','Katia','F','Santos'),
+(7,'Petrópolis','Vera','F','Santos'),
+(8,'Petrópolis','Palmerio','F','Santos');
