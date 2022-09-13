f38bf82d1e13c9ed4f2cb9c4a50233adf3e95c2f
BrunoLFSantos <brunoleonardofsantos@gmail.com>
2022-09-13 18:36:19 -0300

    Implementado opções de json, xml e yaml na api

--- 01_Spring/rest-spring/src/main/java/br/com/brunosantos/controllers/PersonController.java
+++ 01_Spring/rest-spring/src/main/java/br/com/brunosantos/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.brunosantos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.brunosantos.data.vo.v1.PersonVO;
 import br.com.brunosantos.data.vo.v2.PersonVOV2;
 import br.com.brunosantos.services.PersonServices;
+import br.com.brunosantos.util.MediaType;
 
 @RestController
 @RequestMapping("api/person/v1")
@@ -25,33 +25,33 @@ public class PersonController {
 	@Autowired
 	private PersonServices personServices;
 	
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception{
 		return personServices.findById(id);
 	}
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public List<PersonVO> findAll() throws Exception{
 		return personServices.findAll();
 	}
 	
 	@PostMapping( 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public PersonVO create(@RequestBody PersonVO PersonVO) throws Exception{
 		return personServices.create(PersonVO);
 	}
 	
 	@PostMapping(value="/v2",
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 PersonVOV2) throws Exception{
 		return personServices.createV2(PersonVOV2);
 	}
 	
 	@PutMapping( 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
 	public PersonVO update(@RequestBody PersonVO PersonVO) throws Exception{
 		return personServices.update(PersonVO);
 	}
